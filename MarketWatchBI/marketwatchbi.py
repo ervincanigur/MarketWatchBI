a7c988e94a8ed115e6435200892620a2674e5a83
Ervin Canigur <ervin@canigur.com>
2020-01-13 21:38:04 -0600

    import update

--- MarketWatchBI/marketwatchbi.py
+++ MarketWatchBI/marketwatchbi.py
@@ -1,8 +1,13 @@
 import yaml
+# from datetime import datetime
+import sched
+import time
 from MarketWatchBI.scrap import Scrap
 from MarketWatchBI.populate import Populate
 
-if __name__ == "__main__":
+
+def job():
+    print("Starting Job")
     """ Load Config """
     with open("config.yml", "r") as f:
         cfg = yaml.safe_load(f)
@@ -15,3 +20,14 @@ if __name__ == "__main__":
     out = Populate()
     out.write(data)
     out.save(cfg['output_file'])
+
+
+if __name__ == "__main__":
+    # Makes job run every minute, for testing
+    s = sched.scheduler(time.time, time.sleep)
+    s.enter(60, 1, job)
+    s.run()
+    # TODO: add time restrictions, disabled for debug
+    # while True:
+    #     if int(datetime.minute) % 3:  # every 3 minutes
+    #         job()
